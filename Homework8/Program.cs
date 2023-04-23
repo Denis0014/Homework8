33ccbe1e31d3057a7658dfb986c7e2a710cd4ccf
Denis <109104246+Denis0014@users.noreply.github.com>
2023-04-23 21:36:59 +0300

    Task 5

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -26,17 +26,13 @@ namespace Homework8
                 node = null;
                 return;
             }
-            var nodeTemp = new Node(values.First(), null, null);
+            var first = node = new Node(values.First(), null, null);
             foreach (var value in values.Skip(1))
             {
-                nodeTemp.Next = new Node(value, nodeTemp, null);
-                nodeTemp = nodeTemp.Next;
+                node.Next = new Node(value, node, null);
+                node = node.Next;
             }
-            while (nodeTemp.Prev != null)
-            {
-                nodeTemp = nodeTemp.Prev;
-            }
-            node = nodeTemp;
+            node = first;
         }
         public Node First() => node;
         public Node Last()
@@ -67,21 +63,109 @@ namespace Homework8
         }
         public void AddFirst(T value)
         {
+            if (node == null)
+            {
+                node = new Node(value, null, null);
+                return;
+            }
             node.Prev = new Node(value, null, node);
             node = node.Prev;
         }
         public void AddLast(T value)
         {
+            var first = node;
+            if (node == null)
+            {
+                node = new Node(value, null, null);
+                return;
+            }
             while (node.Next != null)
             {
                 node = node.Next;
             }
             node.Next = new Node(value, node, null);
-            while (node.Prev != null)
+            node = first;
+        }
+        public bool Remove(int n)
+        {
+            var first = node;
+            if (n > this.Count() || n < 0)
             {
+                return false;
+            }
+            for (var i = 0; i < n; i++)
+            {
+                node = node.Next;
+            }
+            node.Prev.Next = node.Next;
+            node.Next.Prev = node.Prev;
+            node = first;
+            return true;
+        }
+        public bool Insert(int n, T value)
+        {
+            var first = node;
+            if (n > this.Count() || n < 0)
+            {
+                return false;
+            }
+            for (var i = 0; i < n; i++)
+            {
+                node = node.Next;
+            }
+            var nodeTemp = new Node(value, node, node.Next);
+            node.Next.Prev = nodeTemp;
+            node.Next = nodeTemp;
+            node = first;
+            return true;
+        }
+        public void Reverse()
+        {
+            if (this.Count() == 0)
+            {
+                node = null;
+                return;
+            }
+            var nodeTemp = new Node(this.Last().Value, null, null);
+            node = this.Last().Prev;
+            while (node != null)
+            {
+                nodeTemp.Next = new Node(node.Value, nodeTemp, null);
+                nodeTemp = nodeTemp.Next;
                 node = node.Prev;
             }
+            node = nodeTemp;
+            while (node.Prev != null)
+                node = node.Prev;
         }
+        /* public void ShiftLeft(int n)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                var count = this.Count();
+                var first = node;
+                var nodeTemp = node;
+                node.Prev = this.Last();
+                node = this.Last();
+                node.Next = first;
+                node = first;
+                for (var j = 0; j < count; j++)
+                {
+                    nodeTemp.Next.Prev = node;
+                    nodeTemp.Next = node.Next.Next;
+                    node = node.Next;
+                }
+                node.Next = null;
+                node = first;
+                node.Prev = null;
+                node = first;
+            }
+        } */
+
         public override string ToString()
         {
             var nodeTemp = node;
@@ -90,7 +174,7 @@ namespace Homework8
             {
                 return "<empty>";
             }
-            while (nodeTemp.Next != null) 
+            while (nodeTemp.Next != null)
             {
                 res += nodeTemp.Value + " <-> ";
                 nodeTemp = nodeTemp.Next;
@@ -109,6 +193,14 @@ namespace Homework8
             Console.WriteLine(lst.Count());
             lst.AddFirst(0);
             lst.AddLast(6);
+            Console.WriteLine(lst.ToString());
+            lst.Remove(3);
+            Console.WriteLine(lst.ToString());
+            lst.Insert(2, 3);
+            Console.WriteLine(lst.ToString());
+            // lst.ShiftLeft(3);
+            // Console.WriteLine(lst.ToString());
+            lst.Reverse();
             Console.WriteLine(lst.ToString());
         }
     }
